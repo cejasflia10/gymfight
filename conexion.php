6ba35664a590ed44d634742b88dfa6fbc877b3a9
cejasflia10 <cejasdan24533@gmail.com>
2025-06-11 23:25:05 +0200

    Actualizo conexión y variables

--- conexion.php
+++ conexion.php
@@ -1,13 +1,11 @@
 <?php
-$host = "mysql-railway.internal"; // nombre exacto que te dio Railway
-$usuario = "root";               // usuario (lo vi en tu captura)
-$contraseña = "WkZzSWUaTRzG1vXWoSPaabFDyr1dIzMj"; // tu contraseña real de Railway
-$base_de_datos = "railway";      // nombre de la base de datos
-$puerto = 3306;                  // puerto estándar
+$host = getenv(mysql.railway.internal);
+$usuario = getenv(root);
+$contraseña = getenv(WkzZsWUaTRzGlvXWoSPaabFDyrldIzMj);
+$base_de_datos = getenv(railway);
 
-$conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos, $puerto);
+$conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos);
 
-// Verificar conexión
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
