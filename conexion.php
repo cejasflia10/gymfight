90a5b0126ba49ff670b71cc3ec9c49a5fd93efd7
cejasflia10 <cejasdan24533@gmail.com>
2025-06-11 23:46:46 +0200

    Actualizo conexión y variables

--- conexion.php
+++ conexion.php
@@ -1,15 +1,11 @@
 <?php
-// Leer los parámetros de la base de datos desde variables de entorno
-$host = getenv('mysql.railway.internal');
-$usuario = getenv('root');
-$contraseña = getenv('WkzZsWUaTRzGlvXWoSPaabFDyrldIzMj');
-$base_de_datos = getenv('railway');
-$puerto = getenv('3306') ?: 3306;
+$host = "mysql-railway.internal";
+$usuario = "root";
+$contraseña = "WkZzSWUaTRzG1vXWoSPaabFDyr1dIzMj";
+$base_de_datos = "railway";
+$puerto = 3306;
 
-// Crear la conexión
 $conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos, $puerto);
-
-// Verificar conexión
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
